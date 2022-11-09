7fb96ceff04662747ab08c02cc859d63fda0b0fd
William Kenzie <william@kenzie.one>
2022-11-09 14:32:40 -0600

    Add sound, more keywords, click to respond, and bugfixes
    Signed-off-by: William Kenzie <william@kenzie.one>

--- src/index.js
+++ src/index.js
@@ -2,31 +2,35 @@ class Asher {
     constructor() {
         self.stage = 0;
         self.coping = 0;
-        self.countdown = 60;
+        self.countdown = 300;
         self.timer = setInterval(() => {
             self.countdown -= 1;
             if (self.countdown === 0) {
                 this.sendMessage("stillThere").then(() => {
-                    self.countdown = 60
+                    self.countdown = 300
                 })
             }
-        }, 500)
+        }, 1000)
         self.message = "";
-        self.helpWith = ""
+        self.helpWith = "";
+        self.notificationObject = document.getElementById("notification");
+        self.typingObject = document.getElementById("typing")
 
         self.triggers = {
             suicide: [
                 "die",
                 "kms",
                 "kill myself",
-                "suicid"
+                "suicid",
+                "dying"
             ],
             yes: [
                 "yes",
                 "yeah",
                 "yup",
                 "uh huh",
-                "do"
+                "do ",
+                "pretty much"
             ],
             no: [
                 "no",
@@ -36,6 +40,12 @@ class Asher {
                 "don't",
                 "not",
                 "can't"
+            ],
+            okay: [
+                "okay",
+                "ok",
+                "alright",
+                "alrighty"
             ]
         }
 
@@ -45,6 +55,12 @@ class Asher {
                 "Hello! My name is Asher and my goal is to help you feel better! What seems to be the problem?",
                 "Hey! My name is Asher, I'm here to help! How can I help?",
                 "Greetings to you, I am Asher. How can I help today?"
+            ],
+            okay: [
+                "Okay",
+                "OK",
+                "Alright",
+                "Alrighty"
             ],
             stayCalm: [
                 "It's important to stay calm in a situation like this.",
@@ -90,7 +106,7 @@ class Asher {
                 "A hike or physical activity? You don't have to drop and give me 20, but a short jog or hike through nature can give you a new perspective on life. It may be worth trying?",
                 "Do you happen to be near a park? Swinging on a swing is a great amount of fun and it lets us connect with our inner child! Perhaps try that for a few minutes?",
                 "What kinds of hobbies do you have? Do you think they'd be helpful to you to help you calm down?",
-                "Not to sound like a parent, but maybe cleaning your room will help. Focusing on the effort to organize even a small part of your room may be enough to distract you.",
+                "Maybe cleaning your room will help. Focusing on the effort to organize even a small part of your room may be enough to distract you.",
                 "Are you able to go and get a mint or gum? The flavor of the various mints have been found to help reduce stress, just be sure to focus on the flavor!",
                 "Do you like tea? Sometimes tea can be very calming, depending on you - of course. Might be worth a shot! And if you don't like tea, just remember that there are plenty of flavors out there, ranging" +
                 " from orange flavored tea to peppermint flavored tea, to sweet iced tea - tea is very versatile. And, if tea really isn't for you, there are plenty other coping mechanisms!",
@@ -106,7 +122,25 @@ class Asher {
             stillThere: [
                 "Are you still there?",
                 "Hey just checking in, are you still there?",
-                "*portal turret voice* Are you still there?"
+                "*portal turret voice* Are you still there?",
+                "Are you still online?"
+            ],
+            suicide2: [
+                "It seems coping isn't helping you, so I'm going to escalate this.",
+                "Coping isn't working so I'm going to try something else.",
+                "Alright I'm going to try something else."
+            ],
+            preventionPlanDescribe: [
+                "We're going to build a suicide prevention plan. Relax, it's nothing official, just answer a few questions and hopefully it'll remind you of things you have to live for.",
+                "Let's build a suicide prevention plan. I'll ask some pretty basic questions, and you just need to answer them as truthfully as possible. Together we'll figure out the best course of action from there."
+            ],
+            suicidePreventionPlan: [
+                "Alright, so who are 3 people you'd live for? Friends, family, pets - nothing is off limits.",
+                "Okay, What is your favorite thing to do?",
+                "OK, Who's your best friend?",
+                "Alright, Do you have a therapist you could contact in case of an emergency? It's okay if not.",
+                "Okay, How can we go about limiting your access to potential weapons? Could you, for example, stay the night at a friend's house where you can't access such weapons?",
+                "Who is supportive of you in times of stress?"
             ],
             isThatAll: [
                 "It seems like you're doing better now - is that all for today?",
@@ -121,7 +155,7 @@ class Asher {
     }
 
     async received(message) {
-        self.countdown = 60
+        self.countdown = 300
         self.message = message.toString().toLowerCase().split(" ");
         appendBody(sentTemplate(message));
 
@@ -131,25 +165,30 @@ class Asher {
                     self.helpWith = "suicide";
                     await this.sendMessage("understandingSuicidal");
                     this.changeStage(1);
+                    this.setOptions(["Yes", "No"])
                 }
                 break;
 
             case 1: // Are they suicidal?
                 if (self.helpWith === "suicide" && this.stringContains("yes")) {
                     await this.sendMessage("suicideStep1");
+                    this.setOptions(["Yes", "No"])
                     this.changeStage(2);
                 } else if (self.helpWith === "suicide" && this.stringContains("no")) {
                     await this.sendMessage("wrong");
+                    this.setOptions(["Yes", "No"])
                     this.changeStage(0);
                 }
                 break;
 
             case 2: // Do they have coping skills?
                 if (self.helpWith === "suicide" && this.stringContains("yes")) {
-                    await this.sendMessage("isThatAll");
+                    await this.sendMessage("copingWorking");
+                    this.setOptions(["Yes", "No"])
                     //this.changeStage(0);
                 } else if (self.helpWith === "suicide" && this.stringContains("no")) {
                     await this.sendMessage("getCoping");
+                    this.setOptions(["OK"])
                     this.changeStage(3);
                 }
                 break;
@@ -157,10 +196,12 @@ class Asher {
             case 3: // Has coping skills, do they work?
                 if (self.helpWith === "suicide" && this.stringContains("yes")) {
                     await this.sendMessage("isThatAll");
-                } else if (self.helpWith === "suicide") {
+                    this.setOptions(["Yes", "No"])
+                } else if (self.helpWith === "suicide" && (this.stringContains("no") || this.stringContains("okay"))) {
                     await this.sendMessage("getCoping");
                     await this.sendMessage("coping");
                     await this.sendMessage("help");
+                    this.setOptions(["Yes", "No"])
                     await this.changeStage(4);
                 }
                 break;
@@ -170,18 +211,31 @@ class Asher {
                     if (self.sayings.coping.length > 0) {
                         await this.sendMessage("coping", true);
                         await this.sendMessage("help");
+                        this.setOptions(["Yes", "No"])
                     } else {
                         await this.sendMessage("copingFailed");
+                        await this.sendMessage("suicide2");
+                        await this.sendMessage("suicidePreventionPlan");
+                        await this.sendMessage("preventionPlanDescribe")
                         await this.changeStage(5);
                     }
                 } else if (self.helpWith === "suicide" && this.stringContains("yes")) {
                     await this.sendMessage("isThatAll")
 
                 }
+                break
+
+            case 5: // Coping skills didn't work - try something else
+                if (self.sayings.coping.length > 0) {
+                    await this.sendMessage("suicidePreventionPlan", true, "okay");
+                } else {
+                    await this.sendMessage("help");
+                    this.changeStage(6);
+                }
         }
     }
 
-    async sendMessage(messageCategory, remove = false) {
+    async sendMessage(messageCategory, remove = false, preface = "") {
         function delay(time) {
             return new Promise(resolve => setTimeout(resolve, time));
         }
@@ -189,12 +243,48 @@ class Asher {
         const target = self.sayings[messageCategory];
         const random = Math.floor(Math.random() * target.length);
 
+        let prefaceTarget = "";
+        let prefaceRandom = "";
+
+        if (preface !== "") {
+            prefaceTarget = self.sayings[preface];
+            prefaceRandom = Math.floor(Math.random() * prefaceTarget.length);
+        }
+
+        self.typingObject.hidden = false
         await delay(target[random].length * 15);
-        appendBody(receivedTemplate(target[random]));
+        if (preface !== "") {
+            appendBody(receivedTemplate(prefaceTarget[prefaceRandom] + " " + target[random]));
+        } else {
+            appendBody(receivedTemplate(target[random]));
+        }
         if (remove) self.sayings[messageCategory].splice(random, 1);
+        self.notificationObject.pause()
+        self.notificationObject.play()
+        self.typingObject.hidden = true
     }
 
     changeStage(number) {
         self.stage = number;
+    }
+
+    clearCoping() {
+        self.sayings.coping = [];
+    }
+
+    setOptions(options) {
+        if (options === "textbox") {
+            document.getElementById("userInput").innerHTML =
+                `<input class="flex items-center h-10 w-full rounded px-3 text-sm" id="userInputBox"
+                       placeholder="Type your message…" type="text">`
+        } else {
+            document.getElementById("userInputBox").classList.toggle("hidden")
+            for (const option of options) {
+                document.getElementById("userInput").innerHTML += `
+                <button class="btn bg-fuchsia-400 text-gray-100 p-1 px-3 rounded-full" 
+                onclick="asher.received('${option}'); asher.setOptions('textbox')">${option}</button>
+                `
+            }
+        }
     }
 }
